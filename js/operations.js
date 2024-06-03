338012dfa0128a51f54e9085291ffe4b84162b18
Om21Nigam <123570430+Om21Nigam@users.noreply.github.com>
2024-06-03 04:53:11 +0000

    operators and comparisons

---
+++ js/operations.js
@@ -0,0 +1,23 @@
+ console.log(2**7) // power to 2
+ console.log(1+2+"2"+3+4+5);
+ console.log((2+3)*4%5);
+
+ let x=4n
+ y=x++
+ console.log(`x:${x} y:${y}`);
+ z=++x
+ console.log(`x:${x} y:${z}`);
+
+//********************comparisons******************/
+
+// console.log(2!=0);
+console.log(null>=0);
+console.log(null>0);
+console.log(null==0);
+
+console.log(undefined>=0);
+console.log(undefined>0);
+console.log(undefined==0);
+
+console.log("2"==2); //checks without considering datatypes
+console.log("2"===2); //checks considering datatype
