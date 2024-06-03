08783d7bd27d2ff24bf7a291679e82c33730005a
Om21Nigam <123570430+Om21Nigam@users.noreply.github.com>
2024-06-03 04:25:18 +0000

    conversion of datatypes

---
+++ js/variables.js
@@ -0,0 +1,20 @@
+// let score="32"
+// let scoreInNumber=Number(score)
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+
+
+let num=0
+let nameInstring=Boolean(num)
+console.log(typeof nameInstring)
+console.log(nameInstring)
+
+let num2=null
+let nameInstring2=Number(num)
+console.log(typeof nameInstring2)
+console.log(nameInstring2)
+
+let num3=undefined
+let nameInstring3=String(num)
+console.log(typeof nameInstring3)
+console.log(nameInstring3)
