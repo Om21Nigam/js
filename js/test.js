9fac18c4351f39fda5c8c4cf2614321fddfd9b9c
Om21Nigam <123570430+Om21Nigam@users.noreply.github.com>
2024-06-03 03:43:54 +0000

    datatypes and basics

---
+++ js/test.js
@@ -0,0 +1,23 @@
+"use strict"
+
+
+
+//console.log("om");
+const ok="23abc"
+var v=30
+let c="ojas"
+//alpha="random"
+let okay
+
+//console.log(ok)
+//console.log(typeof(v))
+//console.log(c)
+//console.log(1+2+ alpha+1+" "+ 2)
+//v="30js"
+//console.log(typeof(v))
+//console.log(v)
+//console.log(okay)
+console.log(typeof null);
+console.log(typeof undefined);
+
+
