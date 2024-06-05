6f03857bfd6ceb08c7c1c7af71881f2d2fcf2167
Om21Nigam <123570430+Om21Nigam@users.noreply.github.com>
2024-06-05 07:49:44 +0000

    math and num

---
+++ js/numberandmaths.js
@@ -0,0 +1,31 @@
+const score=new Number(100)
+//console.log(score);
+
+///now it has all properties of number
+
+// console.log(score.toString().length)
+// console.log(typeof score)
+// console.log(score.toFixed(6));
+
+const anothernum = 324.7758
+//console.log(anothernum.toPrecision(2))
+
+// console.log(Math.abs(-9.8));
+// console.log(Math.round(-9.8));
+// console.log(Math.ceil(-9.8));
+// console.log(Math.floor(-9.8));
+// console.log(Math.min(-9.8 , 9, 0));
+// console.log(Math.max(-9.8 , 9, 0));
+console.log(Math.random()); //gives random values between 0 and 1
+
+console.log(Math.floor((Math.random()*10)+1)); //for values btw 1 and 10
+
+
+//for values btw certain range
+
+min=20
+max=40
+
+console.log(Math.floor(Math.random()*(max-min+1))+min) //may treat it as formula
+ 
+
